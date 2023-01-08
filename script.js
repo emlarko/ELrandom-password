badf9d887e46b9e681d2f9918717d29431561d00
Emily Larkin <emilylarkin@Emilys-MacBook-Air.local>
2023-01-08 14:54:24 +0000

    password function

--- script.js
+++ script.js
@@ -1,6 +1,34 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+const char = {
+  alpha: 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ',
+  number: '0123456789',
+  symbol: "!#$%&'()*+,-./:;<=>?@^_`{|}~",
+};
+
+
+function generatePassword() {
+  var passwordChar = char.alpha += "";
+  
+  var length = window.prompt("How many characters?");
+  
+  var numbers = window.confirm("Numbers?");
+    if (numbers) {
+      passwordChar += char.number;
+    }
+  var special = window.confirm("Special characters?");
+    if (special) {
+      passwordChar += char.symbol;
+    }
+    var password = "";
+  for (let i = 0; i < length; i++) {
+    password += passwordChar[Math.floor(Math.random() * passwordChar.length)]
+  }
+  return password;
+  }
+
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
@@ -12,3 +40,4 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
