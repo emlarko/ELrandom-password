29a010b11c4158365fef1ecc88eeba19202aaaa9
Emily Larkin <emilylarkin@Emilys-MacBook-Air.local>
2023-01-10 23:28:15 +0000

    JavaScript edits

--- script.js
+++ script.js
@@ -2,32 +2,58 @@
 var generateBtn = document.querySelector("#generate");
 
 const char = {
-  alpha: 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ',
+  alpha: 'abcdefghijklmnopqrstuvwxyz',
   number: '0123456789',
   symbol: "!#$%&'()*+,-./:;<=>?@^_`{|}~",
 };
 
-
 function generatePassword() {
-  var passwordChar = char.alpha += "";
+  var passwordChar = "";
   
-  var length = window.prompt("How many characters?");
-  
-  var numbers = window.confirm("Numbers?");
-    if (numbers) {
+  var length = window.prompt("How many characters between 8 - 128?");
+
+  if (length >= 8 && length <= 128) {
+
+    var lowercase = window.confirm("Do you want lowercase letters?");
+      if (lowercase) {
+      passwordChar += char.alpha;
+      }
+    
+    var uppercase = window.confirm("Do you want uppercase letters?");
+      if (uppercase) {
+      passwordChar += char.alpha.toUpperCase();
+      }
+
+    var numbers = window.confirm("Do you want numbers?");
+      if (numbers) {
       passwordChar += char.number;
-    }
-  var special = window.confirm("Special characters?");
-    if (special) {
+      }
+      
+    var special = window.confirm("Do you want special characters?");
+      if (special) {
       passwordChar += char.symbol;
     }
-    var password = "";
-  for (let i = 0; i < length; i++) {
-    password += passwordChar[Math.floor(Math.random() * passwordChar.length)]
-  }
-  return password;
-  }
+    // Sends an error message if there are criteria selected.
+    if (passwordChar == "") {
+      window.alert("Please choose at least one criteria.");
+      return "Try again!";
+     }
 
+    var password = "";
+
+    for (let i = 0; i < length; i++) {
+      password += passwordChar[Math.floor(Math.random() * passwordChar.length)];
+    } 
+
+  return password;
+
+  }
+  // Sends an error message if the password is too short or too long.
+  else {
+    window.alert("Incorrect password length, please try again!");
+    return "Try again!";
+  }
+}
 
 // Write password to the #password input
 function writePassword() {
@@ -35,9 +61,7 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-
